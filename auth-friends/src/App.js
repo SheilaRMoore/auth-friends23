d59adc57c607c6cecceb093263a86c88c5a9e197
Sheila R Moore <sherexmykes@github.com>
2020-01-16 22:18:42 -0500

    working app, will add test later

--- auth-friends/src/App.js
+++ auth-friends/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-
+import LoginForm from './components/LoginForm';
+import Display from './components/Display';
+import AddFriendForm from './components/AddFriendForm';
+import PrivateRoute from './components/PrivateRoute';
 
 import './App.css';
 
@@ -8,11 +11,12 @@ function App() {
   return (
     <Router>
       <div>
-        <h1>Welcome To FriendBook</h1>
-        <ul>
+        <h1 style={{color: "Blue"}}> Welcome To FriendBook</h1>
+        <ul style={{backgroundColor: "lightgreen"}}>
+      
           <li><Link to="/login">Log In</Link></li>
           <li><Link to="/friends">Friend List</Link></li>
-          <li><Link to="/addfriend">Friends A Friend</Link></li>
+          <li><Link to="/addfriend">Add A Friend</Link></li>
         </ul>
         <Switch>
           <PrivateRoute path="/friends" component={Display} />
